3ccd0ddfc7abcf864a811dd5c8116263937ce47e
Abir-Hasan-52 <abirhasan5208+github@gmail.com>
2025-04-13 20:41:36 +0600

    fake data create by using chatgpt & console it app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
  
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
 import Navbar from './Components/Navber/Navbar'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
        
       <Navbar></Navbar>
        
+      <Blogs> </Blogs>
     </>
   )
 }
